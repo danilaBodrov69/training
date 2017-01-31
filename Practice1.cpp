5bdc7bd4dbba8ca38f4afebe3b826a22224c7c96
Dandy Bramasta <Dandy Bramasta>
2017-01-31 23:21:09 +0200

    1

---
+++ Practice1.cpp
@@ -0,0 +1,18 @@
+// Practice1.cpp : Defines the entry point for the console application.
+//
+
+#include "stdafx.h"
+#include <stdio.h>
+#include <iostream>
+
+void main()
+{
+	int x = 4;
+	int y = 2;
+	int z = 5;
+	int b;
+
+	b=x*y+z/2;
+	std::cout<<"The Value is:"<< b << std::endl;
+}
+
